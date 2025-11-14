cb4c747e22c2985130dd77eb152de4a8d0410e3f
Arwa Aloraibi <arwa7oraibi@gmail.com>
2025-11-14 14:47:34 +0300

    Added the map function to the albumList to display all albums

--- src/components/AlbumList/AlbumList.jsx
+++ src/components/AlbumList/AlbumList.jsx
@@ -5,9 +5,18 @@ const AlbumList = (props) => {
   return (
     <div>
       <h1>Album List</h1>
-      <div></div>
+      <div>
+         {!props.albums.length ? (
+          <h2>No Albums Yet!</h2>
+        ) : (
+        <ul>
+          {props.albums.map((album) => (
+            <li key={album._id}>{album.name}</li>
+          ))}
+        </ul>
+        )}
+      </div>
     </div>
   );
-};
-
+}
 export default AlbumList;
